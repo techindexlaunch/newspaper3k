9faa62b3addf5b02f3b20ebd67466ec4eb11d7f8
techindexlaunch <techindexlaunch@gmail.com>
2025-04-04 21:02:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -51,3 +51,9 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
